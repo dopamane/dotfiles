889058af214cdb17258de083c93e92193687518c
dopamane <dwc1295@gmail.com>
2021-06-19 17:37:42 -0700

    setup for haskell

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # prerequisites
+sudo add-apt-repository -y ppa:hvr/ghc
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install -y build-essential make vim tmux kitty fonts-firacode
